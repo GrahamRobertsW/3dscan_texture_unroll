574b45ed5dc8dc2b5bb53f86d9088e70ce81c907
Graham Roberts <grahamrobertsw@gmail.com>
2018-04-10 12:47:05 -0400

    added more helper functions, still struggling to find axis of cyllinder

--- unwrap_funcs_and_notes.py
+++ unwrap_funcs_and_notes.py
@@ -78,7 +78,41 @@ def reorient_to_origin(obj, sample_size = 20000, sample_count = 20):
    #not done need to handle dirty dirty runtime
    return
 
-# activate_edit
+def shift_to_origin(bm):
+   coords = find_coordinates(bm)
+   minc = coords.min(axis=0)
+   maxc = coords.max(axis=0)
+   midc = (maxc+minc)/2.
+   bmesh.ops.translate(bm, verts = bm.verts, vec = -1*midc)
+   return
+
+def find_norms(coords):
+   norms = np.zeros((coords.shape[0],1))
+   for i in range(coords.shape[0]):
+      norms[i] = np.sqrt(np.inner(coords[i,:],coords[i,:]))
+   return(norms)
+
+def find_best_line(coords, mid = None):
+   if mid == None:
+      uu, dd, vv = np.linalg.svd(coords)
+   else:
+      uu, dd, vv = np.linalg.svd(coords-mid)
+   return(vv[0])
+
+def find_center_points(coords):
+   norms = find_norms(coords)
+   R = norms.min()
+   closeinds = np.less(norms,mult*R)
+   length = np.sum(closeinds)
+   closecoords = np.zeros((length, 3))
+   index = 0
+   for i in range(len(coords)):
+      if closeinds[i]:
+         closecoords[index] = coords[i]
+         index += 1
+   return(closecoords)
+ 
+# activate_edit   
 # finds the active object from the automatically generated filename
 # sets that to active object and in edit mode
 # just do this first its like super important setup for other stuff
